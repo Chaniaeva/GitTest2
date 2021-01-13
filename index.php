5bc9c23ed700c2f2c5834ce8193723b5967c42fd
Chania <chania.eva@gmail.com>
2021-01-13 13:54:50 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -6,5 +6,7 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure porro repellat sint eum voluptatem aut quo ea eos nulla animi. Eos cupiditate laudantium suscipit itaque officia explicabo dolor soluta totam?</p>
+    <li>satu</li>
+    <li>dua</li>
 </body>
 </html>
